77ba30a15662ac911277d4dee9b7edd4d1c01862
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-03-19 18:23:25 -0600

    week5_hmwk

--- Plus/week_4/week _4_homework/src/index.js
+++ Plus/week_4/week _4_homework/src/index.js
@@ -13,28 +13,32 @@ let hours = now.getHours();
 let minutes = now.getMinutes();
 let dateToday = document.querySelector("#date-now");
 
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
 dateToday.innerHTML = `${day} ${hours}:${minutes}`;
 
 function citySubmit(event) {
-  //works
   event.preventDefault();
   let enterCity = document.querySelector("#search-city");
   let h1 = document.querySelector("h1");
-  h1.innerHTML = `${enterCity.value}`;
+  let cityName = enterCity.value;
+  h1.innerHTML = cityName;
+
+  let key = "fo3a64f446f636fead1d860tcbcd7535";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${key}`;
+
+  axios.get(apiUrl).then(showValue);
 }
-
-let cityForm = document.querySelector("#city-input"); //works
-cityForm.addEventListener("submit", citySubmit);
-
-let key = "fo3a64f446f636fead1d860tcbcd7535";
-let query = document.querySelector("h1"); // Where I am stuck. I need city to be the result of what is entered into the form.
-
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}`;
 
 function showValue(response) {
-  //DOES NOT WORK. Even When query is set to 'Paris'
   let cityTemp = Math.round(response.data.temperature.current);
-  let tempNow = document.querySelector("#currentTempValue");
+  let tempNow = document.querySelector(".currentTempValue");
   tempNow.innerHTML = `${cityTemp}`;
 }
-axios.get(apiUrl).then(showValue);
+
+let cityForm = document.querySelector("#city-input");
+cityForm.addEventListener("submit", citySubmit);
+
+//It was a CLASS VS ID Issue on line 37
